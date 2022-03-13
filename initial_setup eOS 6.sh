89f94f5fcea70ef47fae7cbe4d336e7f74a078d3
tannishpage <tannishpage1@gmail.com>
2022-03-13 19:42:38 +1000

    Added a few more flatpaks and fixed indicator code

--- initial_setup eOS 6.sh
+++ initial_setup eOS 6.sh
@@ -7,15 +7,18 @@ sudo add-apt-repository --yes ppa:papirus/papirus
 sudo add-apt-repository --yes ppa:philip.scott/pantheon-tweaks
 
 sudo apt update
-sudo apt install -y git gcc g++ firefox net-tools vim make vlc papirus-icon-theme terminator texstudio libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 snapd pantheon-tweaks ffmpeg graphviz lm-sensors dconf-editor python3-pip texlive libglib2.0-dev libgranite-dev libindicator3-dev libwingpanel-dev indicator-application
+sudo apt install -y git gcc g++ firefox net-tools vim make vlc papirus-icon-theme terminator texstudio libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 snapd pantheon-tweaks ffmpeg graphviz lm-sensors dconf-editor python3-pip texlive libglib2.0-dev libgranite-dev libindicator3-dev libwingpanel-dev indicator-application kdeconnect
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #flatpak install flathub com.discordapp.Discord
-flatpak install flathub com.slack.Slack
-flatpak install flathub us.zoom.Zoom
-flatpak install flathub io.atom.Atom
-flatpak install flathub com.nextcloud.desktopclient.nextcloud
+flatpak install -y flathub com.slack.Slack
+flatpak install -y flathub us.zoom.Zoom
+flatpak install -y flathub io.atom.Atom
+flatpak install -y flathub com.nextcloud.desktopclient.nextcloud
+flatpak install -y flathub org.qownnotes.QOwnNotes
+flatpak install -y flathub com.github.bajoja.indicator-kdeconnect
+
 
 #wget "https://zoom.us/client/latest/zoom_amd64.deb" -O zoom.deb
 #wget "https://atom.io/download/deb" -O atom.deb
@@ -23,6 +26,7 @@ flatpak install flathub com.nextcloud.desktopclient.nextcloud
 #wget "https://downloads.slack-edge.com/linux_releases/slack-desktop-4.17.0-amd64.deb" -O slack.deb
 wget "https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh" -O anaconda.sh
 
+# Adding support for the indicators for applications such as discord, etc.
 echo "Adding indicator stuff..."
 wget "https://github.com/Lafydev/wingpanel-indicator-ayatana/raw/master/com.github.lafydev.wingpanel-indicator-ayatana_2.0.8_odin.deb" -O indicator.deb
 sudo dpkg -i indicator.deb
@@ -30,6 +34,8 @@ sudo dpkg -i indicator.deb
 mkdir -p ~/.config/autostart
 cp /etc/xdg/autostart/indicator-application.desktop ~/.config/autostart/
 sed -i 's/^OnlyShowIn.*/OnlyShowIn=Unity;GNOME;Pantheon;/' ~/.config/autostart/indicator-application.desktop
+sudo sed -i 's/^OnlyShowIn.*/OnlyShowIn=Unity;GNOME;Pantheon;/' /etc/xdg/autostart/indicator-application.desktop
+
 
 #sudo dpkg -i zoom.deb
 #sudo dpkg -i atom.deb
